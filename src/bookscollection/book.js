316c9770abd654edae9ca92578bb829fdeffdbc9
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-10-07 00:03:56 +0300

    edit book function

--- src/bookscollection/book.js
+++ src/bookscollection/book.js
@@ -1,15 +1,17 @@
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
+import './books.css'
 
-function Book ({id, title, image, author, description}) {
-    const params = useParams();
+function Book({ id, title, image, author, description }) {
+    // const params = useParams();
 
     return (
         <div>
-            <img src={image} height={150} />
-            <h2> {title} </h2>
-            <h4> {author} </h4>
-            <p>{description}</p>
-            <button type="button" onClick={like} >Like | {likes}</button>
+            <div className="booksContainer">
+                <img src={image} height={150} alt={title} />
+                <h5> {title} </h5>
+                <h6> By: {author} </h6>
+                <p>{description}</p>
+            </div>
         </div>
     );
 };
