9602972882c28db42f25e4cba5fead400adfddad
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-10-07 05:26:15 +0300

    add delete book button

--- src/bookscollection/book.js
+++ src/bookscollection/book.js
@@ -1,19 +1,28 @@
-// import { useParams } from "react-router-dom";
 import './books.css'
 
-function Book({ id, title, image, author, description }) {
-    // const params = useParams();
+function Book({ book, setBooks, title, image, author, description }) {
+
+    function handleDelete () {
+        fetch(`http://localhost:3000/books/${book.id}`, {
+            method: "DELETE",
+        })
+        .then((resp) => resp.json())
+        .then(() => {
+            setBooks((books) => books.filter((selectBook) => selectBook.id !== book.id));
+        });
+    }
 
     return (
         <div className="booksContainer">
             <div>
-                <img src={image} height={150} alt="image" />
+                <img src={image} height={130} alt="book" />
                 <h5> {title} </h5>
                 <h6> By: {author} </h6>
                 <p>{description}</p>
             </div>
-            <div className="buttons">
-                <button type="button" class="btn btn-primary">READ</button>
+            <div className="d-grid gap-2 d-md-flex justify-content-md-between">
+                <button className="btn btn-primary" type="button">Read</button>
+                <button className="btn btn-secondary" type="button" onClick={handleDelete} >Delete</button>
             </div>
         </div>
     );
